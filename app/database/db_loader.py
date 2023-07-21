2ecb02c72c90f232cd47e279bdfab568ea373550
a-menshikov <a.menshikov1989@gmail.com>
2023-07-21 17:41:15 +0300

    Update db_loader.py

--- app/database/db_loader.py
+++ app/database/db_loader.py
@@ -1,6 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 conn_url = 'postgresql+psycopg2://artem:ahea31ahea31@database/menu_db'
 engine = create_engine(conn_url)
 Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
