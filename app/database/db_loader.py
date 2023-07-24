5783b328cf9f4b7521b218711acb81260435f1a3
a-menshikov <a.menshikov1989@gmail.com>
2023-07-24 10:13:49 +0300

    переменные вынесены в .env

--- app/database/db_loader.py
+++ app/database/db_loader.py
@@ -1,8 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-conn_url = 'postgresql+psycopg2://artem:ahea31ahea31@database/menu_db'
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+
+conn_url = (f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
+            f'@database/{POSTGRES_DB}')
 engine = create_engine(conn_url)
 Base = declarative_base()
 
