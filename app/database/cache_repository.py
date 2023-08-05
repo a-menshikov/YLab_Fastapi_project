f43a512ee53e8e1f43176ca1b394631f5bcc1c3a
a-menshikov <a.menshikov1989@gmail.com>
2023-08-05 13:48:51 +0300

    кэш репозиторий

---
+++ app/database/cache_repository.py
@@ -0,0 +1,149 @@
+import pickle
+
+from fastapi import Depends
+
+from app.api.dishes.crud_repository import DishRepository
+from app.api.menus.crud_repository import MenuRepository
+from app.api.submenus.crud_repository import SubmenuRepository
+from app.database.db_loader import EXPIRATION, redis
+
+
+class СacheRepository():
+    def __init__(self, menu_repo: MenuRepository = Depends(),
+                 submenu_repo: SubmenuRepository = Depends(),
+                 dish_repo: DishRepository = Depends()) -> None:
+        self.cacher = redis
+        self.menu_repo = menu_repo
+        self.submenu_repo = submenu_repo
+        self.dish_repo = dish_repo
+
+    def set_all_dishes_cache(self, submenu_id, items):
+        redis.set(f'all_dishes_{submenu_id}',
+                  pickle.dumps(items), ex=EXPIRATION)
+
+    def get_all_dishes_cache(self, submenu_id):
+        cache = redis.get(f'all_dishes_{submenu_id}')
+        if cache:
+            items = pickle.loads(cache)
+            return items
+        return None
+
+    def set_dish_cache(self, item):
+        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+
+    def get_dish_cache(self, id):
+        cache = redis.get(id)
+        if cache:
+            item = pickle.loads(cache)
+            return item
+        return None
+
+    def create_dish_cache(self, item):
+        self.delete_all_dish_cache(str(item.submenu_id))
+        redis.delete(str(item.submenu_id))
+        submenu = self.submenu_repo.get_submenu_by_id(id=item.submenu_id)
+        self.delete_all_submenu_cache(str(submenu.menu_id))
+        redis.delete(str(submenu.menu_id))
+        self.delete_all_menu_cache()
+        self.set_dish_cache(item)
+
+    def update_dish_cache(self, item):
+        self.delete_all_dish_cache(str(item.submenu_id))
+        self.set_dish_cache(item)
+
+    def delete_dish_cache(self, dish_id):
+        redis.delete(dish_id)
+        dish_object = self.dish_repo.get_dish_by_id(id=dish_id)
+        self.delete_all_dish_cache(str(dish_object.submenu_id))
+        redis.delete(str(dish_object.submenu_id))
+        submenu = self.submenu_repo.get_submenu_by_id(
+            id=dish_object.submenu_id
+        )
+        self.delete_all_submenu_cache(str(submenu.menu_id))
+        redis.delete(str(submenu.menu_id))
+        self.delete_all_menu_cache()
+
+    def delete_all_dish_cache(self, submenu_id):
+        redis.delete(f'all_dishes_{submenu_id}')
+
+    def set_all_submenus_cache(self, menu_id, items):
+        redis.set(f'all_submenus_{menu_id}',
+                  pickle.dumps(items), ex=EXPIRATION)
+
+    def get_all_submenus_cache(self, menu_id):
+        cache = redis.get(f'all_submenus_{menu_id}')
+        if cache:
+            items = pickle.loads(cache)
+            return items
+        return None
+
+    def set_submenu_cache(self, item):
+        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+
+    def get_submenu_cache(self, id):
+        cache = redis.get(id)
+        if cache:
+            item = pickle.loads(cache)
+            return item
+        return None
+
+    def create_submenu_cache(self, item):
+        self.delete_menu_cache(str(item.menu_id))
+        self.delete_all_menu_cache()
+        self.delete_all_submenu_cache(str(item.menu_id))
+        self.set_submenu_cache(item)
+
+    def update_submenu_cache(self, item):
+        self.delete_all_submenu_cache(str(item.menu_id))
+        self.set_submenu_cache(item)
+
+    def delete_submenu_cache(self, submenu_id):
+        redis.delete(submenu_id)
+        self.delete_all_dish_cache(submenu_id)
+        submenu_object = self.submenu_repo.get_submenu_by_id(
+            id=submenu_id
+        )
+        self.delete_all_submenu_cache(str(submenu_object.menu_id))
+        dishes = self.dish_repo.get_all_dishes(submenu_id)
+        if dishes:
+            for dish in dishes:
+                self.delete_dish_cache(str(dish.id))
+
+    def delete_all_submenu_cache(self, menu_id):
+        redis.delete(f'all_submenus_{menu_id}')
+
+    def set_all_menus_cache(self, items):
+        redis.set('all_menus', pickle.dumps(items), ex=EXPIRATION)
+
+    def get_all_menus_cache(self):
+        cache = redis.get('all_menus')
+        if cache:
+            items = pickle.loads(cache)
+            return items
+        return None
+
+    def set_menu_cache(self, item):
+        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+
+    def get_menu_cache(self, id):
+        cache = redis.get(id)
+        if cache:
+            item = pickle.loads(cache)
+            return item
+        return None
+
+    def create_update_menu_cache(self, item):
+        self.delete_all_menu_cache()
+        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+
+    def delete_all_menu_cache(self):
+        redis.delete('all_menus')
+
+    def delete_menu_cache(self, menu_id):
+        redis.delete(menu_id)
+        self.delete_all_menu_cache()
+        self.delete_all_submenu_cache(menu_id)
+        submenus = self.submenu_repo.get_all_submenus(menu_id)
+        if submenus:
+            for submenu in submenus:
+                self.delete_submenu_cache(str(submenu.id))
