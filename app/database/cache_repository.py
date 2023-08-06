fe2e639f07ac7469ffb440eaa872e078aa9ce42e
a-menshikov <a.menshikov1989@gmail.com>
2023-08-06 11:02:55 +0300

    соединение с редис через пул, добавление зависимости в кэш репозиторий, добавление зависимости в майпай

--- app/database/cache_repository.py
+++ app/database/cache_repository.py
@@ -1,11 +1,12 @@
 import pickle
 
 from fastapi import Depends
+from redis import Redis
 
 from app.api.dishes.crud_repository import DishRepository
 from app.api.menus.crud_repository import MenuRepository
 from app.api.submenus.crud_repository import SubmenuRepository
-from app.database.db_loader import EXPIRATION, redis
+from app.database.db_loader import EXPIRATION, get_redis
 from app.database.models import Dish, Menu, Submenu
 
 
@@ -14,20 +15,21 @@ class СacheRepository():
 
     def __init__(self, menu_repo: MenuRepository = Depends(),
                  submenu_repo: SubmenuRepository = Depends(),
-                 dish_repo: DishRepository = Depends()) -> None:
-        self.cacher = redis
+                 dish_repo: DishRepository = Depends(),
+                 cacher: Redis = Depends(get_redis)) -> None:
+        self.cacher = cacher
         self.menu_repo = menu_repo
         self.submenu_repo = submenu_repo
         self.dish_repo = dish_repo
 
     def set_all_dishes_cache(self, submenu_id: str, items: list[Dish]) -> None:
         """Запись всех блюд в кеш."""
-        redis.set(f'all_dishes_{submenu_id}',
-                  pickle.dumps(items), ex=EXPIRATION)
+        self.cacher.set(f'all_dishes_{submenu_id}',
+                        pickle.dumps(items), ex=EXPIRATION)
 
     def get_all_dishes_cache(self, submenu_id: str) -> list[Dish] | None:
         """Получение всех блюд из кеша."""
-        cache = redis.get(f'all_dishes_{submenu_id}')
+        cache = self.cacher.get(f'all_dishes_{submenu_id}')
         if cache:
             items = pickle.loads(cache)
             return items
@@ -35,11 +37,11 @@ class СacheRepository():
 
     def set_dish_cache(self, item: Dish) -> None:
         """Запись блюда в кеш."""
-        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+        self.cacher.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
 
     def get_dish_cache(self, id: str) -> Dish | None:
         """Получение блюда из кеша."""
-        cache = redis.get(id)
+        cache = self.cacher.get(id)
         if cache:
             item = pickle.loads(cache)
             return item
@@ -48,9 +50,9 @@ class СacheRepository():
     def delete_related_dish_cache(self, submenu_id: str, menu_id: str) -> None:
         """Удаление кэша связанных с блюдом объектов."""
         self.delete_all_dish_cache(submenu_id)
-        redis.delete(submenu_id)
+        self.cacher.delete(submenu_id)
         self.delete_all_submenu_cache(menu_id)
-        redis.delete(menu_id)
+        self.cacher.delete(menu_id)
         self.delete_all_menu_cache()
 
     def create_dish_cache(self, item: Dish, submenu_id: str,
@@ -67,22 +69,22 @@ class СacheRepository():
     def delete_dish_cache(self, dish_id: str, submenu_id: str,
                           menu_id: str) -> None:
         """Работа с кэшем при удалении блюда."""
-        redis.delete(dish_id)
+        self.cacher.delete(dish_id)
         self.delete_related_dish_cache(submenu_id, menu_id)
 
     def delete_all_dish_cache(self, submenu_id: str) -> None:
         """Удаление всех блюд подменю из кеша."""
-        redis.delete(f'all_dishes_{submenu_id}')
+        self.cacher.delete(f'all_dishes_{submenu_id}')
 
     def set_all_submenus_cache(self, menu_id: str,
                                items: list[Submenu]) -> None:
         """Запись всех подменю в кеш."""
-        redis.set(f'all_submenus_{menu_id}',
-                  pickle.dumps(items), ex=EXPIRATION)
+        self.cacher.set(f'all_submenus_{menu_id}',
+                        pickle.dumps(items), ex=EXPIRATION)
 
     def get_all_submenus_cache(self, menu_id: str) -> list[Submenu] | None:
         """Получение всех подменю из кеша."""
-        cache = redis.get(f'all_submenus_{menu_id}')
+        cache = self.cacher.get(f'all_submenus_{menu_id}')
         if cache:
             items = pickle.loads(cache)
             return items
@@ -90,11 +92,11 @@ class СacheRepository():
 
     def set_submenu_cache(self, item: Submenu) -> None:
         """Запись подменю в кеш."""
-        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+        self.cacher.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
 
     def get_submenu_cache(self, id: str) -> Submenu | None:
         """Получение подменю из кеша."""
-        cache = redis.get(id)
+        cache = self.cacher.get(id)
         if cache:
             item = pickle.loads(cache)
             return item
@@ -114,7 +116,7 @@ class СacheRepository():
 
     def delete_submenu_cache(self, submenu_id: str, menu_id: str) -> None:
         """Работа с кэшем при удалении подменю."""
-        redis.delete(submenu_id)
+        self.cacher.delete(submenu_id)
         self.delete_all_dish_cache(submenu_id)
         self.delete_all_submenu_cache(menu_id)
         dishes = self.dish_repo.get_all_dishes(submenu_id)
@@ -124,15 +126,15 @@ class СacheRepository():
 
     def delete_all_submenu_cache(self, menu_id: str) -> None:
         """Удаление всех подменю меню из кеша."""
-        redis.delete(f'all_submenus_{menu_id}')
+        self.cacher.delete(f'all_submenus_{menu_id}')
 
     def set_all_menus_cache(self, items: list[Menu]) -> None:
         """Запись всех меню в кеш."""
-        redis.set('all_menus', pickle.dumps(items), ex=EXPIRATION)
+        self.cacher.set('all_menus', pickle.dumps(items), ex=EXPIRATION)
 
     def get_all_menus_cache(self) -> list[Menu] | None:
         """Получение всех меню из кеша."""
-        cache = redis.get('all_menus')
+        cache = self.cacher.get('all_menus')
         if cache:
             items = pickle.loads(cache)
             return items
@@ -140,11 +142,11 @@ class СacheRepository():
 
     def set_menu_cache(self, item: Menu) -> None:
         """Запись меню в кеш."""
-        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+        self.cacher.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
 
     def get_menu_cache(self, id: str) -> Menu | None:
         """Получение меню из кеша."""
-        cache = redis.get(id)
+        cache = self.cacher.get(id)
         if cache:
             item = pickle.loads(cache)
             return item
@@ -153,15 +155,15 @@ class СacheRepository():
     def create_update_menu_cache(self, item: Menu) -> None:
         """Работа с кэшем при создании нового меню."""
         self.delete_all_menu_cache()
-        redis.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
+        self.cacher.set(str(item.id), pickle.dumps(item), ex=EXPIRATION)
 
     def delete_all_menu_cache(self) -> None:
         """Удаление всех меню из кеша."""
-        redis.delete('all_menus')
+        self.cacher.delete('all_menus')
 
     def delete_menu_cache(self, menu_id: str) -> None:
         """Работа с кэшем при удалении меню."""
-        redis.delete(menu_id)
+        self.cacher.delete(menu_id)
         self.delete_all_menu_cache()
         self.delete_all_submenu_cache(menu_id)
         submenus = self.submenu_repo.get_all_submenus(menu_id)
--- app/database/db_loader.py
+++ app/database/db_loader.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from redis import Redis  # type: ignore
+from redis import ConnectionPool, Redis
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,7 +13,8 @@ POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = os.getenv('REDIS_PORT')
-EXPIRATION = os.getenv('EXPIRATION')
+EXPIRATION = 3600
+REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
 
 conn_url = (f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
             f'@database/{POSTGRES_DB}')
@@ -32,8 +33,14 @@ def get_db():
         db.close()
 
 
-redis = Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    db=0,
-)
+def create_redis():
+    """Создание пула соединений с Redis."""
+    return ConnectionPool.from_url(REDIS_URL)
+
+
+pool = create_redis()
+
+
+def get_redis():
+    """Возвращает соединение с Redis."""
+    return Redis(connection_pool=pool)
