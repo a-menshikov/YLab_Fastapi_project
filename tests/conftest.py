b9d99243bcda2b326f2fe20946ea64ddb7cfc3fa
a-menshikov <a.menshikov1989@gmail.com>
2023-08-11 11:34:39 +0300

    для тестового прогона

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,23 @@
+import os
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+
+conn_url = (f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
+            f'@database/{POSTGRES_DB}')
+test_engine = create_async_engine(conn_url)
+
+TestAsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False,
+                                     bind=test_engine, class_=AsyncSession)
+
 pytest_plugins = 'tests.fixtures'
+
+
+async def override_db():
+    """Возвращает соединение с базой данных."""
+    async with TestAsyncSessionLocal() as async_session:
+        yield async_session
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,7 +4,9 @@ from typing import Any
 import pytest
 from httpx import AsyncClient
 
+from app.database.db_loader import Base, get_db
 from app.main import app
+from tests.conftest import override_db, test_engine
 
 
 @pytest.fixture(scope='session')
@@ -16,9 +18,17 @@ def event_loop(request):
     loop.close()
 
 
+@pytest.fixture(autouse=True, scope='function')
+async def init_db():
+    """Создание таблиц."""
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 @pytest.fixture(scope='session')
 async def client():
     """Асинхронный клиент."""
+    app.dependency_overrides = {get_db: override_db}
     async with AsyncClient(app=app, base_url='http://test') as client:
         yield client
 
